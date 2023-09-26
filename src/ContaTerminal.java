b6fd9249e5fa321fdc60e5e2c606bdda8c32686c
victoralbucorde <126472403+victoralbucorde@users.noreply.github.com>
2023-09-26 10:39:59 -0300

    Update ContaTerminal.java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -13,11 +13,14 @@ public class ContaTerminal{
 
         System.out.println("Digite seu nome");
         String nome = scanner.next();
+        
         System.out.println("Digite seu sobrenome");
         String sobrenome = scanner.next();
+        
         System.out.println("Digite o saldo inicial da conta.");
         double saldo = scanner.nextDouble();
-
+        
         System.out.println("Olá " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + numeroAgencia + " e seu saldo " + saldo + " já está disponível para saque");
+        scanner.close();
     }
 }
